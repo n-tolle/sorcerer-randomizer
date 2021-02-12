8158285e5737293f58af5baf847dd6bf5a1f9567
Nicholas Tolle <nbtolle@gmail.com>
2021-02-12 09:57:59 -0700

    Complete seeding script

--- db/seed.js
+++ db/seed.js
@@ -1,37 +1,36 @@
 const seedData = require('./characterData');
 
 let seedOptions = function() {
-  let characters = ['Ariapses', 'Tegu', 'Miselda', 'Zevrane', 'Virgiliu', 'Seth', 'Jaleesa', 'Raganhar', 'Thenoch', 'Wachiwi', 'Sigismund', 'Tatsu', 'Merlin', 'Nicodemus'];
-  let lineages = ['Animist', 'Demonologist', 'Bloodlord', 'Necromancer', 'Druid', 'House of Petro', 'Children of Alyisia', 'Sword of Gabriel'];
-  let domains = ['Haunted Forest', 'Forgotten Temple', 'Outcast Sanctuary', 'Screaming Coast', 'Bloodsoaked Fjord', 'Forsaken Church', 'Lunatic Asylum', 'Royal Palace', 'Witch Mountain', 'Bewitched Woodland', 'Bloodcursed Ship', 'Forlorn Opera House'];
-  let boards = ['London ~ Whitechapel', 'London ~ Southwark', 'London ~ Old London', 'Underground ~ Hive', 'Luxor ~ Karnak Temple', 'Cairo ~ Old City', 'Giza ~ Necropolis'];
+  let options = [{type: 'characters', name: 'Ariapses'}, {type: 'characters', name: 'Tegu'}, {type: 'characters', name: 'Miselda'}, {type: 'characters', name: 'Zevrane'}, {type: 'characters', name: 'Virgiliu'}, {type: 'characters', name: 'Seth'}, {type: 'characters', name: 'Jaleesa'}, {type: 'characters', name: 'Raganhar'}, {type: 'characters', name: 'Thenoch'}, {type: 'characters', name: 'Wachiwi'}, {type: 'characters', name: 'Sigismund'}, {type: 'characters', name: 'Tatsu'}, {type: 'characters', name: 'Merlin'}, {type: 'characters', name: 'Nicodemus'}, {type: 'lineages', name: 'Animist'}, {type: 'lineages', name: 'Demonologist'}, {type: 'lineages', name: 'Bloodlord'}, {type: 'lineages', name: 'Necromancer'}, {type: 'lineages', name: 'Druid'}, {type: 'lineages', name: 'House of Petro'}, {type: 'lineages', name: 'Children of Alyisia'}, {type: 'lineages', name: 'Sword of Gabriel'}, {type: 'domains', name: 'Haunted Forest'}, {type: 'domains', name: 'Forgotten Temple'}, {type: 'domains', name: 'Outcast Sanctuary'}, {type: 'domains', name: 'Screaming Coast'}, {type: 'domains', name: 'Bloodsoaked Fjord'}, {type: 'domains', name: 'Forsaken Church'}, {type: 'domains', name: 'Lunatic Asylum'}, {type: 'domains', name: 'Royal Palace'}, {type: 'domains', name: 'Witch Mountain'}, {type: 'domains', name: 'Bewitched Woodland'}, {type: 'domains', name: 'Bloodcursed Ship'}, {type: 'domains', name: 'Forlorn Opera House'}, {type: 'boards', name: 'London ~ Whitechapel'}, {type: 'boards', name: 'London ~ Southwark'}, {type: 'boards', name: 'London ~ Old London'}, {type: 'boards', name: 'Underground ~ Hive'}, {type: 'boards', name: 'Luxor ~ Karnak Temple'}, {type: 'boards', name: 'Cairo ~ Old City'}, {type: 'boards', name: 'Giza ~ Necropolis'}];
 
-  let characterCompletion = 0;
-  for (let i = 0; i < characters.length; i++) {
-    seedData.determineExistence(characters[i], (err, found) => {
+  let completion = 0;
+  for (let i = 0; i < options.length; i++) {
+    seedData.determineExistence(options[i].name, (err, found) => {
       if (err) {
         console.log('Problem Determining Existence: ', err);
       } else {
         if (!found) {
-          seedData.save({type: characters, name: characters[i], selected: false}, (err, characterOption) => {
+          seedData.save({type: options[i].type, name: options[i].name, selected: false}, (err, characterOption) => {
             if (err) {
               console.log('Problem seeding character: ', err);
             } else {
-              characterCompletion++;
-              if (characterCompletion === characters.length) {
-                console.log('Character Seeding Complete');
+              completion++;
+              if (completion === options.length) {
+                console.log('Seeding Complete');
+                return;
               }
             }
           })
         } else {
-          characterCompletion++;
-          if (characterCompletion === characters.length) {
-            console.log('Character Seeding Complete');
+          completion++;
+          if (completion === options.length) {
+            console.log('Seeding Complete');
+            return;
           }
         }
       }
     })
   }
-}
+};
 
 seedOptions();
